59ebc1ef72c927e1ac406eadd158e7bdcab5e1b5
Nicolo41 <72193849+Nicolo41@users.noreply.github.com>
2024-04-09 14:12:50 +0200

    homepage

---
+++ compagneur/components/containers/Pcomponent/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Pcomponent() {
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <div className="flex h-10 w-10 bg-white rounded-xl"></div>
+    </div>
+  );
+}
